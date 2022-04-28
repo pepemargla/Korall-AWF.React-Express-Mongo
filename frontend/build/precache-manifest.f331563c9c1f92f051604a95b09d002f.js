ada0e6c025eb2812f0ed7eb0d2702ddbf002cf55
Pepe Martinez <josedejesus.martinez@gmail.com>
2022-04-28 16:06:10 +0200

    TEST/0.0.0 build

---
+++ frontend/build/precache-manifest.f331563c9c1f92f051604a95b09d002f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8ada706dc6ddd84557d019cb86d0c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "955fb4c82eb551a3556c",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e1636c1efe33a72f4e45",
+    "url": "./static/css/main.90566059.chunk.css"
+  },
+  {
+    "revision": "955fb4c82eb551a3556c",
+    "url": "./static/js/2.f2f8083c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.f2f8083c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1636c1efe33a72f4e45",
+    "url": "./static/js/main.d7374b98.chunk.js"
+  },
+  {
+    "revision": "7faa1d9684cd0f870caf",
+    "url": "./static/js/runtime-main.e263c96d.js"
+  }
+]);
